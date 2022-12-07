235717bf7ef9dcbde55897aa70841c0e01820596
spandana <spandana.nimma154@gmail.com>
2022-12-07 13:10:35 +0530

    im in mood for v1.3.1

--- 07_IM_IN_A_MOOD_FOR/07_IM_IN_A_MOOD_FOR.ino
+++ 07_IM_IN_A_MOOD_FOR/07_IM_IN_A_MOOD_FOR.ino
@@ -6,35 +6,41 @@
 
 #include "LiquidCrystal_I2C.h"
 // Global variables
-byte switch_val      = 0;
-byte prev_switch_val = 0;
-byte reply           = 0; 
-const byte SWITCH_PIN = 2  ;
+byte IR_val      = 0;
+byte prev_IR_val = 1;
+byte reply           = 0; // varies sfrom 0 to 7
+const byte IR_PIN = 2  ;
 String Msg1 = "SOHAM";
 String Msg2 = "IM IN A MOOD FOR";
 LiquidCrystal_I2C Lcd(0x27, 16, 2);
 int Count = 0 ;
 
-
-void setup() {
-  Lcd.begin();
-  pinMode(SWITCH_PIN, INPUT_PULLUP);
-  // intro message
+void printMsg(String msg1 , String msg2) {
   Lcd.clear();
   Lcd.setCursor(0, 0);
-  Lcd.print(Msg1);
+  Lcd.print(msg1);
   Lcd.setCursor(0, 1);
-  Lcd.print(Msg2);
+  Lcd.print(msg2);
   delay(500);
 }
 
-void loop() {
-  switch_val = digitalRead(SWITCH_PIN);
-  if ((switch_val != prev_switch_val) && (switch_val == HIGH)) {
+void setup() {
+  Lcd.begin();
+  Serial.begin(9600);
+  pinMode(IR_PIN, INPUT);
+  // intro message
+  printMsg(Msg1, Msg2);
+}
+
+void loop()  {
+IR_val = digitalRead(IR_PIN);
+  Serial.println(IR_val);
+  if ((prev_IR_val == 1) && (IR_val == LOW)) {
     if (Count == 5) {
       Lcd.clear();
       Lcd.setCursor(0, 0);
       Lcd.print("PLEASE BE QUIET,");
+      Serial.println(" I AM NOT IN A MOOD");
       Lcd.setCursor(0, 1);
       Lcd.print(" I'M NOT IN MOOD");
       Count++;
@@ -44,6 +50,7 @@ void loop() {
       Lcd.clear();
       Lcd.setCursor(0, 0);
       Lcd.print(" I AM IN A MOOD");
+      Serial.println(" I AM IN A MOOD");
       Lcd.setCursor(0, 1);
       //    delay(1000);
       switch (reply) {
@@ -97,13 +104,14 @@ void loop() {
           break;
       }
     }
-      delay(2500); // wait for 2.5 sec 
+    while(!digitalRead(IR_PIN));
+//      delay(2500); // wait for 2.5 sec 
       Lcd.clear(); // the display is cleared
-      Lcd.setCursor(0, 0); 
+      Lcd.setCursor(0, 0); //set the cursor position at the column = 0 , row = 0 
       Lcd.print(Msg1);
-      Lcd.setCursor(0, 1); 
+      Lcd.setCursor(0, 1); //set the cursor position at the column = 0 , row = 1 
       Lcd.print(Msg2);
       delay(500);
   }
-  prev_switch_val = switch_val;
+   prev_IR_val = IR_val;
 }
