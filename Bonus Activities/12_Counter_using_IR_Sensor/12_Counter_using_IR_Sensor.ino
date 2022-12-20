5c560ccee4d4fefaf8f19827c0162f510e8cda5f
ganeshkumarjammu <shreeganeshkumar2k@gmail.com>
2022-12-20 15:58:10 +0530

    Counter using IR Sensor

--- Bonus Activities/12_Counter_using_IR_Sensor/12_Counter_using_IR_Sensor.ino
+++ Bonus Activities/12_Counter_using_IR_Sensor/12_Counter_using_IR_Sensor.ino
@@ -3,165 +3,202 @@
   Organisation: Soham Academy Of Human Excellence
   Website: https://ria.sohamacademy.org/
 ****************************************************/
+int  aSegment = 10;
+int  bSegment = 11;
+int  cSegment = 5;
+int  dSegment = 6;
+int  eSegment = 7;
+int  fSegment = 9;
+int  gSegment = 8;
+int irPin = 2; 
+int time = 100;
+int t = 0;
 
-#define aPin 8  //        
-#define bPin 9  //      _____
-#define cPin 3  //     |  A  |
-#define dPin 4  //   F |_____| B
-#define ePin 5  //     |  G  |
-#define fPin 7  //   E |_____| C
-#define gPin 6  //        D     O dot
-#define irPin 2
+boolean irState = LOW ;
+boolean prevState = HIGH ;
 
-boolean On = HIGH;  //<On=0; for Common anode> <On=1; for Common cathode>
-boolean Off = LOW;
-boolean IRState = true ;
-boolean prevIRState = true ;
-
-int count = 0;
-
-void setup() {
+void setup()
+{
   Serial.begin(9600);
-  Serial.println("Welcome to Soham Acadmey of Human Excellence");
-  pinMode(aPin, OUTPUT);
-  pinMode(bPin, OUTPUT);
-  pinMode(cPin, OUTPUT);
-  pinMode(dPin, OUTPUT);
-  pinMode(ePin, OUTPUT);
-  pinMode(fPin, OUTPUT);
-  pinMode(gPin, OUTPUT);
-  pinMode(irPin, INPUT);
-  zero();
+  Serial.print("IR state:");
+  Serial.println(digitalRead(irPin));
+
+  pinMode(aSegment, OUTPUT);
+  pinMode(bSegment, OUTPUT);
+  pinMode(cSegment, OUTPUT);
+  pinMode(dSegment, OUTPUT);
+  pinMode(eSegment, OUTPUT);
+  pinMode(fSegment, OUTPUT);
+  pinMode(gSegment, OUTPUT);
+  pinMode( irPin, INPUT);
 }
 
-void loop()  {
-  IRState = digitalRead(irPin);
-  Serial.println(IRState);
-  if ((prevIRState == 1) && (IRState == LOW)) {
-   count++;
-  }
-  if(count < 9){
-    count++;
-    showNumber(count);
+void loop()
+{
+  Serial.println(digitalRead(irPin));
+  irState = digitalRead(irPin);
+  if ( !irState && prevState) {
+    t++;
+    delay(100);
+    prevState = irState ;
   }
   else{
-    count = 0;
-    showNumber(count); 
+    prevState = irState ;
   }
-  prevIRState = IRState;
-}
 
-void showNumber(int x) {
-  switch (x){
-    case 1: one();   break;
-    case 2: two();   break;
-    case 3: three(); break;
-    case 4: four();  break;
-    case 5: five();  break;
-    case 6: six();   break;
-    case 7: seven(); break;
-    case 8: eight(); break;
-    case 9: nine();  break;
-    case 0: zero(); break;
+  if (t == 0) {
+    pattern0();
+  }
+  if (t == 1) {
+    pattern1();
+  }
+  if (t == 2) {
+    pattern2();
+  }
+  if (t == 3) {
+    pattern3();
+  }
+  if (t == 4) {
+    pattern4();
+  }
+  if (t == 5) {
+    pattern5();
+  }
+  if (t == 6) {
+    pattern6();
+  }
+  if (t == 7) {
+    pattern7();
+  }
+  if (t == 8) {
+    pattern8();
+  }
+  if (t == 9) {
+    pattern9();
+  }
+  if (t == 10) {
+    pattern10();
   }
 }
-
-void one() {
-  digitalWrite( aPin, Off); //
-  digitalWrite( bPin, On);  //      |
-  digitalWrite( cPin, On);  //      |
-  digitalWrite( dPin, Off); //      |
-  digitalWrite( ePin, Off); //      |
-  digitalWrite( fPin, Off);
-  digitalWrite( gPin, Off);
+void pattern0() {
+  digitalWrite(aSegment, HIGH);
+  digitalWrite(bSegment, HIGH);
+  digitalWrite(cSegment, HIGH);
+  digitalWrite(dSegment, HIGH);
+  digitalWrite(eSegment, HIGH);
+  digitalWrite(fSegment, HIGH);
+  digitalWrite(gSegment, LOW);
+  delay(time);
+}
+void pattern1() {
+  digitalWrite(aSegment, LOW);
+  digitalWrite(bSegment, HIGH);
+  digitalWrite(cSegment, HIGH);
+  digitalWrite(dSegment, LOW);
+  digitalWrite(eSegment, LOW);
+  digitalWrite(fSegment, LOW);
+  digitalWrite(gSegment, LOW);
+  delay(time);
 }
 
-void two() {
-  digitalWrite( aPin, On);  //  ____
-  digitalWrite( bPin, On);  //      |
-  digitalWrite( cPin, Off); //  ____|
-  digitalWrite( dPin, On);  // |
-  digitalWrite( ePin, On);  // |____
-  digitalWrite( fPin, Off);
-  digitalWrite( gPin, On);
+void pattern2() {
+  digitalWrite(aSegment, HIGH);
+  digitalWrite(bSegment, HIGH);
+  digitalWrite(cSegment, LOW);
+  digitalWrite(dSegment, HIGH);
+  digitalWrite(eSegment, HIGH);
+  digitalWrite(fSegment, LOW);
+  digitalWrite(gSegment, HIGH);
+  delay(time);
 }
 
-void three() {
-  digitalWrite( aPin, On);  //  ____
-  digitalWrite( bPin, On);  //      |
-  digitalWrite( cPin, On);  //  ____|
-  digitalWrite( dPin, On);  //      |
-  digitalWrite( ePin, Off); //  ____|
-  digitalWrite( fPin, Off);
-  digitalWrite( gPin, On);
+void pattern3() {
+  digitalWrite(aSegment, HIGH);
+  digitalWrite(bSegment, HIGH);
+  digitalWrite(cSegment, HIGH);
+  digitalWrite(dSegment, HIGH);
+  digitalWrite(eSegment, LOW);
+  digitalWrite(fSegment, LOW);
+  digitalWrite(gSegment, HIGH);
+  delay(time);
 }
 
-void four() {
-  digitalWrite( aPin, Off); //
-  digitalWrite( bPin, On);  // |    |
-  digitalWrite( cPin, On);  // |____|
-  digitalWrite( dPin, Off); //      |
-  digitalWrite( ePin, Off); //      |
-  digitalWrite( fPin, On);
-  digitalWrite( gPin, On);
+void pattern4() {
+  digitalWrite(aSegment, LOW);
+  digitalWrite(bSegment, HIGH);
+  digitalWrite(cSegment, HIGH);
+  digitalWrite(dSegment, LOW);
+  digitalWrite(eSegment, LOW);
+  digitalWrite(fSegment, HIGH);
+  digitalWrite(gSegment, HIGH);
+  delay(time);
 }
 
-void five() {
-  digitalWrite( aPin, On);  //  ____
-  digitalWrite( bPin, Off); // |
-  digitalWrite( cPin, On);  // |____
-  digitalWrite( dPin, On);  //      |
-  digitalWrite( ePin, Off); //  ____|
-  digitalWrite( fPin, On);
-  digitalWrite( gPin, On);
+void pattern5() {
+  digitalWrite(aSegment, HIGH);
+  digitalWrite(bSegment, LOW);
+  digitalWrite(cSegment, HIGH);
+  digitalWrite(dSegment, HIGH);
+  digitalWrite(eSegment, LOW);
+  digitalWrite(fSegment, HIGH);
+  digitalWrite(gSegment, HIGH);
+  delay(time);
 }
 
-void six() {
-  digitalWrite( aPin, On);  //  ____
-  digitalWrite( bPin, Off); // |
-  digitalWrite( cPin, On);  // |____
-  digitalWrite( dPin, On);  // |    |
-  digitalWrite( ePin, On);  // |____|
-  digitalWrite( fPin, On);
-  digitalWrite( gPin, On);
+void pattern6() {
+  digitalWrite(aSegment, HIGH);
+  digitalWrite(bSegment, LOW);
+  digitalWrite(cSegment, HIGH);
+  digitalWrite(dSegment, HIGH);
+  digitalWrite(eSegment, HIGH);
+  digitalWrite(fSegment, HIGH);
+  digitalWrite(gSegment, HIGH);
+  delay(time);
 }
 
-void seven() {
-  digitalWrite( aPin, On);  //  ____
-  digitalWrite( bPin, On);  //      |
-  digitalWrite( cPin, On);  //      |
-  digitalWrite( dPin, Off); //      |
-  digitalWrite( ePin, Off); //      |
-  digitalWrite( fPin, Off);
-  digitalWrite( gPin, Off);
+void pattern7() {
+  digitalWrite(aSegment, HIGH);
+  digitalWrite(bSegment, HIGH);
+  digitalWrite(cSegment, HIGH);
+  digitalWrite(dSegment, LOW);
+  digitalWrite(eSegment, LOW);
+  digitalWrite(fSegment, LOW);
+  digitalWrite(gSegment, LOW);
+  delay(time);
 }
 
-void eight() {
-  digitalWrite( aPin, On); //  ____
-  digitalWrite( bPin, On); // |    |
-  digitalWrite( cPin, On); // |____|
-  digitalWrite( dPin, On); // |    |
-  digitalWrite( ePin, On); // |____|
-  digitalWrite( fPin, On);
-  digitalWrite( gPin, On);
+void pattern8() {
+  digitalWrite(aSegment, HIGH);
+  digitalWrite(bSegment, HIGH);
+  digitalWrite(cSegment, HIGH);
+  digitalWrite(dSegment, HIGH);
+  digitalWrite(eSegment, HIGH);
+  digitalWrite(fSegment, HIGH);
+  digitalWrite(gSegment, HIGH);
+  delay(time);
 }
 
-void nine() {
-  digitalWrite( aPin, On);  //  ____
-  digitalWrite( bPin, On);  // |    |
-  digitalWrite( cPin, On);  // |____|
-  digitalWrite( dPin, On);  //      |
-  digitalWrite( ePin, Off); //  ____|
-  digitalWrite( fPin, On);
-  digitalWrite( gPin, On);
+void pattern9() {
+  digitalWrite(aSegment, HIGH);
+  digitalWrite(bSegment, HIGH);
+  digitalWrite(cSegment, HIGH);
+  digitalWrite(dSegment, HIGH);
+  digitalWrite(eSegment, LOW);
+  digitalWrite(fSegment, HIGH);
+  digitalWrite(gSegment, HIGH);
+  delay(time);
+}
+void pattern10() {
+  digitalWrite(aSegment, LOW);
+  digitalWrite(bSegment, LOW);
+  digitalWrite(cSegment, LOW);
+  digitalWrite(dSegment, LOW);
+  digitalWrite(eSegment, LOW);
+  digitalWrite(fSegment, LOW);
+  digitalWrite(gSegment, LOW);
+  delay(time);
+  t = 0;
 }
 
-void zero() {
-  digitalWrite( aPin, On); //  ____
-  digitalWrite( bPin, On); // |    |
-  digitalWrite( cPin, On); // |    |
-  digitalWrite( dPin, On); // |    |
-  digitalWrite( ePin, On); // |____|
-  digitalWrite( fPin, On);
-  digitalWrite( gPin, Off);
-}
+
+                             
